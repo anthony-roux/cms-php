4cc222b0e46cf218aac0c6b4269b4d1dfb888d30
anthony.roux <roux.anthony@hetic.net>
2021-12-01 16:27:42 +0100

    add bootstrap

--- app/vendor/composer/installed.php
+++ app/vendor/composer/installed.php
@@ -6,7 +6,7 @@
     'aliases' => 
     array (
     ),
-    'reference' => 'a9cd1af104aeac84f79a36b6ce2fd9b5a2de28fd',
+    'reference' => 'c08f14ea16de385695fc28fafffb997a676d269a',
     'name' => 'anthony.roux/app',
   ),
   'versions' => 
@@ -18,7 +18,23 @@
       'aliases' => 
       array (
       ),
-      'reference' => 'a9cd1af104aeac84f79a36b6ce2fd9b5a2de28fd',
+      'reference' => 'c08f14ea16de385695fc28fafffb997a676d269a',
+    ),
+    'twbs/bootstrap' => 
+    array (
+      'pretty_version' => 'v5.1.3',
+      'version' => '5.1.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '1a6fdfae6be09b09eaced8f0e442ca6f7680a61e',
+    ),
+    'twitter/bootstrap' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v5.1.3',
+      ),
     ),
   ),
 );
