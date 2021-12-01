d8513a350590007d92eb48d733775b4f960330bd
anthony.roux <roux.anthony@hetic.net>
2021-12-01 14:42:09 +0100

    dependencies packages composers

---
+++ app/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'a9cd1af104aeac84f79a36b6ce2fd9b5a2de28fd',
+    'name' => 'anthony.roux/app',
+  ),
+  'versions' => 
+  array (
+    'anthony.roux/app' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a9cd1af104aeac84f79a36b6ce2fd9b5a2de28fd',
+    ),
+  ),
+);
