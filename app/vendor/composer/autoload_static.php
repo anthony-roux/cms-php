dfa39ac97a2845ae08b62debc2833c451386f5fe
Hanna <hanna.achab@hetic.net>
2021-12-03 11:39:14 +0100

    fix: remove bootstrap from composer.lock file

--- app/vendor/composer/autoload_static.php
+++ app/vendor/composer/autoload_static.php
@@ -20,16 +20,11 @@ class ComposerStaticInit09b4b3fe130c479e2db5cf1b30ba9f86
         ),
     );
 
-    public static $classMap = array (
-        'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
-    );
-
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInit09b4b3fe130c479e2db5cf1b30ba9f86::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInit09b4b3fe130c479e2db5cf1b30ba9f86::$prefixDirsPsr4;
-            $loader->classMap = ComposerStaticInit09b4b3fe130c479e2db5cf1b30ba9f86::$classMap;
 
         }, null, ClassLoader::class);
     }
